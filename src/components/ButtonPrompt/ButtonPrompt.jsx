894002ceac034c750cedfd538547054780ef8be2
Melissa Autumn <all@melissaautumn.com>
2022-01-18 19:22:28 -0800

    Consolidate some duplicated sass, and some minor clean-ups to button prompt

--- src/components/ButtonPrompt/ButtonPrompt.jsx
+++ src/components/ButtonPrompt/ButtonPrompt.jsx
@@ -6,9 +6,9 @@ import {Border} from "../Border/Border";
 /**
  * Primary UI component for user interaction
  */
-export const ButtonPrompt = ({ label, labelPosition, ButtonPromptType, ...props }) => {
+export const ButtonPrompt = ({ label, ...props }) => {
     return (
-        <Border>
+        <Border className="button-prompt-border">
             <div className="button-prompt" {...props}>
                 <span className="label">{label}</span>
             </div>
@@ -17,23 +17,10 @@ export const ButtonPrompt = ({ label, labelPosition, ButtonPromptType, ...props 
 };
 
 ButtonPrompt.propTypes = {
-    // Max value for this ButtonPrompt
-    max: PropTypes.number,
-    // Current value of this ButtonPrompt
-    value: PropTypes.number,
-
     // Label for the ButtonPrompt
     label: PropTypes.string,
-
-    labelPosition: PropTypes.oneOf(['left', 'right']),
-
-    ButtonPromptType: PropTypes.oneOf(['solid', 'pips']),
 };
 
 ButtonPrompt.defaultProps = {
-    value: 0,
-    max: 100,
     label: '',
-    labelPosition: 'left',
-    ButtonPromptType: 'solid',
 };
